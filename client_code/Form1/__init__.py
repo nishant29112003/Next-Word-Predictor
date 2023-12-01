12eb24d6bdee88b2699f08103880d67d02b96bbf
nishanttheratpally@gmail.com <nishanttheratpally@gmail.com>
2023-12-01 09:51:00 +0000

    Created Form1; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
